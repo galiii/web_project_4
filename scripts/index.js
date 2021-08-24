0daa0c4d0e812665376939d9c735e195c5944f10
galiii <gali.brill.1408@gmail.com>
2021-08-24 17:04:30 +0300

    card1 having a more dynamic cards by templete element lesson 7 and dom collections lesson 10

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,36 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
 
-//Find me the editButton in the dom
+//Let's find  the cards list in the dom
+const carsdsList = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+
+//Let's find  the editButton in the dom
 let editButton = document.querySelector('.profile__edit-button');
 
 let popup = document.querySelector('.popup');
@@ -16,6 +47,44 @@ let inputJobElement = formElement.querySelector('.form__input_type_job');
 
 let userNameElement = document.querySelector('.profile__name');
 let userJobElement =  document.querySelector('.profile__job');
+
+
+
+function createCard(card) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  //image
+  const imageCardElement = cardElement.querySelector('.card__image');
+  imageCardElement.src = `${card.link}`;
+  imageCardElement.alt = card.name;
+  console.dir(imageCardElement);
+
+  //for h2
+  const cardTitleElement = cardElement.querySelector('.card__title');
+  cardTitleElement.textContent = card.name;
+  //console.log(card);
+
+  //like
+  const likeButtonElement = cardElement.querySelector('.card__like');
+  likeButtonElement.addEventListener("click", function(evt){
+    console.log(evt.target);
+    evt.target.classList.toggle("card__like_active");
+  });
+
+
+  return cardElement;
+}
+
+initialCards.forEach((card, index) => {
+  console.log(index);
+  const cardElement = createCard(card);
+
+  if(cardElement === null || cardElement === undefined) {
+    console.log('Somthing went worng');
+  }
+  carsdsList.prepend(cardElement);
+});
+
+
 
 
 function closePopupEditor() {
@@ -61,6 +130,9 @@ function handleFormSubmit(evt) {
   // i'm still using this function because only in the click event you told me it's redundant
   toggleEditor();
   }
+
+
+
 
 editButton.addEventListener('click', openPopupEditor);
 closeButton.addEventListener('click', closePopupEditor);

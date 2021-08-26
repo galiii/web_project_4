81b517e88758a57de7e35410ade1f7faca4907e7
galiii <gali.brill.1408@gmail.com>
2021-08-26 09:35:04 +0300

    feature/bem adding more files in bem style

--- scripts/index.js
+++ scripts/index.js
@@ -29,7 +29,6 @@ const initialCards = [
 const editProfileButton = document.querySelector('.profile__edit-button');
 const addCardButton = document.querySelector('.profile__add-button'); //change the ben to card
 
-
 //Card
 const list = document.querySelector('.cards__list');
 const templateListItem = document.querySelector('#card-template').content.querySelector('.card');
@@ -42,34 +41,39 @@ const addCardModel = document.querySelector('.popup_type_add-card');
 const editModelCloseButton = editProfileModel.querySelector('.popup__close-button');
 const addCardModelCloseButton = addCardModel.querySelector('.popup__close-button');
 
-
 //Form
 const editProfileForm = editProfileModel.querySelector('.form');
 const addCardForm = addCardModel.querySelector('.form');
 
-//input
+//Input
 const profileNameInput = editProfileModel.querySelector('.form__input_type_name');
 const profileJobInput = editProfileModel.querySelector('.form__input_type_job');
 const cardTitleInput = addCardModel.querySelector('.form__input_type_card-title');
 const cardLinkInput = addCardModel.querySelector('.form__input_type_card-link');
 
-
+//User
 const userNameElement = document.querySelector('.profile__name');
 const userJobElement =  document.querySelector('.profile__job');
 
 
+//function
+const closePopup = (model) => {model.classList.remove('popup_open')};
 
-function generateCard(cardData) {
+const openPopup = (model) => {model.classList.add('popup_open');}
+
+const  toggleModel = (model) => {!model.classList.contains('popup_open') ? openPopup(model) : closePopup(model);}
+
+
+const  generateCard = (cardData) => {
   const listItem = templateListItem.cloneNode(true);
+   /** Items **/
   const image = listItem.querySelector('.card__image');
-  const figureModel = document.querySelector('.popup_type_image');
-
-  const figureCloseButton = figureModel.querySelector('.popup__close-button');
-   console.dir(figureCloseButton);
-
   const deleteButton = listItem.querySelector('.card__delete');
   const title = listItem.querySelector('.card__title');
   const like = listItem.querySelector('.card__like');
+  /** Popuos Items **/
+  const figureModel = document.querySelector('.popup_type_image');
+  const figureCloseButton = figureModel.querySelector('.popup__close-button');
 
   /** Image Property setup **/
   image.src = cardData.link;
@@ -78,93 +82,72 @@ function generateCard(cardData) {
   title.textContent = cardData.name;
 
   /** Delete Property click Event **/
-  deleteButton.addEventListener('click', function(){
-    const lI = deleteButton.closest(".card");
-    lI.remove();
+  deleteButton.addEventListener('click',() =>{
+    const card = deleteButton.closest(".card");
+    card.remove();
   });
   /** Like Property click Event **/
-  like.addEventListener('click', function(evt){
-    console.log(evt.target);
-    evt.target.classList.toggle("card__like_active");
-  });
+  like.addEventListener('click', (evt) => {evt.target.classList.toggle("card__like_active");});
   /** Image Property click Event **/
-  image.addEventListener('click', function(evt){
-    console.log(evt.target);
+  image.addEventListener('click',() => {
+    //console.log(evt.target);
     const img = figureModel.querySelector('.popup__image');
     const caption = figureModel.querySelector('.popup__caption');
-
     img.src = cardData.link;
-    console.dir(caption);
+    //console.dir(caption);
     caption.ariaLabel = cardData.name;
     caption.textContent = cardData.name;
+    //toggleModel(figureModel);
+    //console.log("hello 1");
     figureModel.classList.add('popup_open');
-    //evt.target.classList.toggle("card__like_active");
   });
 
-  figureCloseButton.addEventListener('click', function() {
+  figureCloseButton.addEventListener('click', () => {
+    //console.log("hello");
+    //toggleModel(figureModel);
     figureModel.classList.remove('popup_open');
   });
-
-
-  console.dir(listItem);
+  //console.dir(listItem);
   list.append(listItem);
 }
 
 initialCards.forEach(generateCard);
 
 
-function closePopup(model) {
-  model.classList.remove('popup_open');
+/** Form Submit functions **/
+
+const  addCardFormSubmit = (evt) => {
+  evt.preventDefault();
+  generateCard({name: cardTitleInput.value, link:  cardLinkInput.value});
+  toggleModel(addCardModel);
+  //console.dir(addCardForm);
+  //reset the values (clean)
+  addCardForm.reset();
+  cardTitleInput.value ='';
+  cardLinkInput.value ='';
 }
 
-
-function openPopup(model) {
-  model.classList.add('popup_open');
-}
-
-
-function toggleModel(model) {
-  !model.classList.contains('popup_open') ? openPopup(model) : closePopup(model);
-}
-
-
-function editProfileFormSubmit(evt) {
+const editProfileFormSubmit = (evt) => {
   evt.preventDefault();
   userNameElement.textContent = profileNameInput.value;
   userJobElement.textContent =  profileJobInput.value;
-
-  // i'm still using this function because only in the click event you told me it's redundant
   toggleModel(editProfileModel);
   }
 
 
-  function addCardFormSubmit(evt) {
-    evt.preventDefault();
-    generateCard({name: cardTitleInput.value, link:  cardLinkInput.value});
-    toggleModel(addCardModel);
-    console.dir(addCardForm);
-    //reset the values (clean)
-    addCardForm.reset();
-    cardTitleInput.value = "";
-    cardLinkInput.value ="";
-    }
-
-
-
-/** open  **/
+/** Events **/
+/** Open  **/
 editProfileButton.addEventListener('click', ()=> {
   toggleModel(editProfileModel);
   profileNameInput.value = userNameElement.textContent;
   profileJobInput.value = userJobElement.textContent;
 });
 
-
 addCardButton.addEventListener('click', () => {
   toggleModel(addCardModel);
 });
 
-
-/** close  **/
+/** Close  **/
 editModelCloseButton.addEventListener('click', () => {
   toggleModel(editProfileModel);
 });
@@ -173,6 +156,6 @@ addCardModelCloseButton.addEventListener('click', () => {
   toggleModel(addCardModel);
 });
 
-
+/** Submit **/
 editProfileForm.addEventListener('submit', editProfileFormSubmit);
 addCardForm.addEventListener('submit', addCardFormSubmit);

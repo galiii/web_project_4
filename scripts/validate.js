7744a418ebd539a78fbcd4e96400b579682f3d62
galiii <gali.brill.1408@gmail.com>
2021-09-01 10:48:49 +0300

    a genereal starts

---
+++ scripts/validate.js
@@ -0,0 +1,17 @@
+
+
+
+
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+const settings = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible"
+};
+
+
+enableValidation(settings);

de8c27f158ff7e4f8d38b5042b55765efd2ee2b3
galiii <gali.brill.1408@gmail.com>
2021-09-02 14:48:42 +0300

    feature/show-error adding function and style for error inputs

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,27 +1,32 @@
-
-
-const showError = () => {
-  console.log('Errors ');
+const showError = (input) => {
+  const errorMessage = input.validationMessage;
+  errorElement = document.querySelector(`#${input.id}-error`);
+  input.classList.add("form__input_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
 };
 
-const hideError = () => {
-  console.log('valid');
+const hideError = (input) => {
+  console.log("valid");
+  errorElement = document.querySelector(`#${input.id}-error`);
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+  input.classList.remove("form__input_error");
 };
 
 const checkValidity = (input) => {
-  if(!input.validity.valid) {
-    //show Error
-  }
-  else {
-    //hide Error
+  if (!input.validity.valid) {
+    console.log("show Error");
+    showError(input);
+  } else {
+    hideError(input);
   }
 };
-
 
 // enabling validation by calling enableValidation()
 const enableValidation = (settings) => {
   //find all forms
-  const forms = Array.form(document.querySelectorAll(".form"));
+  const forms = Array.from(document.querySelectorAll(".form"));
   console.log("all form", forms);
 
   //prevent their default behaviour
@@ -29,19 +34,17 @@ const enableValidation = (settings) => {
     console.log(form);
     form.addEventListener("submit", (evt) => evt.preventDefault());
 
-    const inputs = Array.form(document.querySelectorAll(".form__input"));
-  });
-
-  //Subscribe to its change
-  inputs.forEach((input) => {
-    input.addEventListener("input", () => {
-      // check validity
-      checkValidity(input);
-      //toggle Button State
-      //toggleButtonState();
+    const inputs = Array.from(document.querySelectorAll(".form__input"));
+    //Subscribe to its change
+    inputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        // check validity
+        checkValidity(input);
+        //toggle Button State
+        //toggleButtonState();
+      });
     });
   });
-
 
   //check is inputs is valid
   //if it is valid
